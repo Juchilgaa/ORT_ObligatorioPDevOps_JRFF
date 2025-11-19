292335950fcdc6d8917c08434392419135e16d60
Fabian Ferreira <ferreiracigliutti@gmail.com>
2025-11-18 21:00:50 -0300

    Carga incial para despliegue de instancias

---
+++ scripts/03_create_ec2_web.py.py
@@ -0,0 +1,68 @@
+import boto3
+import time
+
+ec2 = boto3.client('ec2')
+ssm = boto3.client('ssm')
+
+# Parte 1: Crear una instancia EC2 asociada al Instance Profile del rol LabRole
+response = ec2.run_instances(
+    ImageId='ami-06b21ccaeff8cd686',
+    MinCount=1,
+    MaxCount=1,
+    InstanceType='t2.micro',
+    IamInstanceProfile={'Name': 'LabInstanceProfile'},
+)
+instance_id = response['Instances'][0]['InstanceId']
+print(f"Instancia creada con ID: {instance_id}")
+
+# Esperar a que la instancia esté en estado running
+ec2.get_waiter('instance_status_ok').wait(InstanceIds=[instance_id])
+
+# Parte 2: Enviar comando y extraer resultado
+command = 'echo "Hello world"'
+response = ssm.send_command(
+    InstanceIds=[instance_id],
+    DocumentName="AWS-RunShellScript",
+    Parameters={'commands': [command]}
+)
+command_id = response['Command']['CommandId']
+
+# Esperar resultado
+while True:
+    output = ssm.get_command_invocation(CommandId=command_id, InstanceId=instance_id)
+    if output['Status'] in ['Success', 'Failed', 'Cancelled', 'TimedOut']:
+        break
+    time.sleep(2)
+print("Output:")
+print(output['StandardOutputContent'])
+
+
+
+import boto3
+
+ec2 = boto3.client('ec2')
+
+user_data = '''#!/bin/bash
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+echo "¡Sitio personalizado!" > /var/www/html/index.html
+'''
+
+response = ec2.run_instances(
+    ImageId='ami-06b21ccaeff8cd686',
+    InstanceType='t2.micro',
+    MinCount=1,
+    MaxCount=1,
+    IamInstanceProfile={'Name': 'LabInstanceProfile'},
+    UserData=user_data
+)
+
+# Agregar tag Name: webserver-devops
+instance_id = response['Instances'][0]['InstanceId']
+ec2.create_tags(
+    Resources=[instance_id],
+    Tags=[{'Key': 'Name', 'Value': 'webserver-devops'}]
+)
+print(f"Instancia creada con ID: {instance_id} y tag 'webserver-devops'")
